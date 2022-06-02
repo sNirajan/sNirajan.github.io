57c9ebbd15d9edbc542fa8163074c16a41a98de0
Nirajan Shah <98066597+sNirajan@users.noreply.github.com>
2022-06-02 12:55:56 -0500

    Update message.php

--- message.php
+++ message.php
@@ -2,14 +2,14 @@
 <?php
   $name = htmlspecialchars($_POST['name']);
   $email = htmlspecialchars($_POST['email']);
-  $subject = htmlspecialchars($_POST['subject']);
+  $phone = htmlspecialchars($_POST['phone']);
   $website = htmlspecialchars($_POST['website']);
   $message = htmlspecialchars($_POST['message']);
   if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $receiver = "mail@nirajanshah.com"; 
+      $receiver = "mail@nirajanshah.com";
       $subject = "From: $name <$email>";
-      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage: $message\n\nRegards,\n$name";
+      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: $email";
       if(mail($receiver, $subject, $body, $sender)){
          echo "Your message has been sent";
